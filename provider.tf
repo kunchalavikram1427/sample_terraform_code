2dc39c179bd23bd633e14230888c57942e75d759
Vikram Babu <kunchalavikram1427@gmail.com>
2025-03-11 08:05:22 +0530

    Update provider.tf

--- provider.tf
+++ provider.tf
@@ -5,12 +5,6 @@ terraform {
       version = "5.22.0"
     }
   }
-  backend "s3" {
-    bucket         = "dme-statefile-bucket"
-    region         = "us-east-1"
-    key            = "dev/state/terraform.tfstate"
-    dynamodb_table = "dme-statefile-table"
-  }
 }
 
 provider "aws" {
